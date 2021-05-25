d10cec3ce28167da96f1259fca9b1e4c29dda4cf
krogean <anna1.kroger@gmail.com>
2021-05-25 10:24:30 +0300

    html template extends

--- oneTemplate/kokeillaanMuotteja.py
+++ oneTemplate/kokeillaanMuotteja.py
@@ -6,5 +6,9 @@ def index():
 	fruits = ["apple", "pear", "peach"]
 	return render_template("base.html", name= "Anna",fruits=fruits)
 
+@app.route("/another")
+def another():
+	return render_template("another.html")
+
 app.run()
 
