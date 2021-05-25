e5e76dd1f18bb3757212912a979085a545d10647
krogean <anna1.kroger@gmail.com>
2021-05-25 11:40:36 +0300

    if in template

--- oneTemplate/kokeillaanMuotteja.py
+++ oneTemplate/kokeillaanMuotteja.py
@@ -4,11 +4,16 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	fruits = ["apple", "pear", "peach"]
-	return render_template("base.html", name= "AnNa",fruits=fruits)
+	return render_template("base.html", name="anna", fruits=fruits)
 
 @app.route("/another")
 def another():
 	return render_template("another.html")
+
+@app.route("/maths")
+def maths():
+	number = 1
+	return render_template("maths.html", number=number)
 
 app.run()
 
