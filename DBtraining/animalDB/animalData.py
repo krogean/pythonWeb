7f7ae00b41c16e3b99487c9c33454fbc7f186824
krogean <anna1.kroger@gmail.com>
2021-05-26 14:29:06 +0300

    folder fix

--- DBtraining/animalDB/animalData.py
+++ DBtraining/animalDB/animalData.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms.ext.sqlalchemy.orm import model_form
@@ -12,6 +12,7 @@ class Animal(db.Model):
 	species = db.Column(db.String, nullable=False)
 	name = db.Column(db.String, nullable=True)
 	age = db.Column(db.Integer, nullable=False)
+	colour = db.Column(db.String, nullable=True)
 
 AnimalForm = model_form(Animal, base_class=FlaskForm, db_session=db.session)
 
@@ -21,24 +22,29 @@ def initMe():
 
 	animal = Animal(species="hamster", name="Hamtaro", age="1")
 	db.session.add(animal)
-	
 	animal = Animal(species="human", name="Dude", age="90")
 	db.session.add(animal)
-
 	animal = Animal(species="lion", name="Simba", age="11")
 	db.session.add(animal)
 
 	db.session.commit()
 	
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
 	animals = Animal.query.all()
+	return render_template("animalList.html", animals=animals)
 
+@app.route("/new", methods=["GET", "POST"])
+def addAnimal():
 	animalForm = AnimalForm()
-	print(request.form)	
-	
-	return render_template("animalList.html", animals=animals, form=animalForm)
+	print(request.form)
+	return render_template("animalForm.html", form=animalForm)
+
+@app.route("/msg")
+def msgPage():
+	flash("Here is a message for you!!!")
+	return redirect("/")
 
 if __name__ == "__main__":
 	app.run()
